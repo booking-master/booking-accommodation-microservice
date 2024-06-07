a781b1a23b2c8ceba0c926cc85b642011d1dbd82
Krsmanovic Djordje (uif72067) <djordje1499@gmail.com>
2024-06-07 16:21:01 +0200

    feat: change database to Accommodation database

---
+++ Booking.Accommodation.Service/src/Booking.Accomodation.Infrastructure/Migrations/20240607141507_NewMigration.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.AccommodationNS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservation_Accommodation_AccomodationId",
+                schema: "accomodaton",
+                table: "Reservation");
+
+            migrationBuilder.RenameColumn(
+                name: "AccomodationId",
+                schema: "accomodaton",
+                table: "Reservation",
+                newName: "AccommodationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reservation_AccomodationId",
+                schema: "accomodaton",
+                table: "Reservation",
+                newName: "IX_Reservation_AccommodationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservation_Accommodation_AccommodationId",
+                schema: "accomodaton",
+                table: "Reservation",
+                column: "AccommodationId",
+                principalSchema: "accomodaton",
+                principalTable: "Accommodation",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservation_Accommodation_AccommodationId",
+                schema: "accomodaton",
+                table: "Reservation");
+
+            migrationBuilder.RenameColumn(
+                name: "AccommodationId",
+                schema: "accomodaton",
+                table: "Reservation",
+                newName: "AccomodationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reservation_AccommodationId",
+                schema: "accomodaton",
+                table: "Reservation",
+                newName: "IX_Reservation_AccomodationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservation_Accommodation_AccomodationId",
+                schema: "accomodaton",
+                table: "Reservation",
+                column: "AccomodationId",
+                principalSchema: "accomodaton",
+                principalTable: "Accommodation",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
